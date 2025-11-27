7a6e712ccf4c3e429908d341079b717a58e178c9
Doctorus Automation <automation@doctorus.local>
2025-11-27 11:55:04 +0100

    feat(operations): export all labels from the labels module

--- src/operations/index.ts
+++ src/operations/index.ts
@@ -1,4 +1,5 @@
 export { Resource, MEDICAL_RESOURCES, PUBLIC_RESOURCES, isMedicalResource, isPublicResource } from './resources';
 export { Action } from './actions';
 export { Operation, OPERATION, getResourceFromOperation, getActionFromOperation } from './operation';
+export * from './labels';
 export { Operations, getAllOperations, getOperationsByResource, getOperationsByAction } from './predefined';
